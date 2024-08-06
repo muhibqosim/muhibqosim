ed76be68ced92c5397a133b269ed356df71d7ff1
muhibqosim <64584529+muhibqosim@users.noreply.github.com>
2024-08-06 10:06:43 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+//Footer
+document.addEventListener('DOMContentLoaded', function() {
+            var currentYear = new Date().getFullYear();
+            document.getElementById('copyright-year').textContent = currentYear;
+        });
 
 function myFunction(x) {
             x.classList.toggle("change");
@@ -83,3 +88,56 @@ function myFunction(x) {
   var shareIcon = document.getElementById("share-icon");
   shareIcon.innerHTML = '<i class="fas fa-share-alt" onclick="openShareDialog()"></i>';
 
+
+//slider Islamic quotes
+let slideIndex = 1;
+
+function showSlide(n) {
+  const slides = document.querySelectorAll('.slide-a');
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
+}
+
+function changeSlide(n) {
+  showSlide((slideIndex += n));
+}
+
+function autoAdvance() {
+  changeSlide(1);
+  setTimeout(autoAdvance, 3000); // 10 seconds
+}
+
+showSlide(slideIndex);
+autoAdvance();
+
+
+
+//JS PDF DONWLOADER
+window.onload = function() {
+    document.getElementById('download-pdf').addEventListener('click', function() {
+        setTimeout(() => {
+            html2canvas(document.getElementById('content')).then(function(canvas) {
+                const doc = new jsPDF.jsPDF({
+                    orientation: 'portrait',
+                    unit: 'pt',
+                    format: 'a4'
+                });
+
+                const imgData = canvas.toDataURL('image/png');
+                const imgProps = doc.getImageProperties(imgData);
+                const pdfWidth = doc.internal.pageSize.getWidth();
+                const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+                
+                doc.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+                doc.save('downloaded-page.pdf');
+            });
+        }, ); // Delay to ensure all content has loaded, adjust time as necessary
+    });
+};
+
+
+//
